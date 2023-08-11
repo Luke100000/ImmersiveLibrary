5bcf3093ce0722faa6941036dd76fc394de07502
Luke100000 <jonas.peche@aon.at>
2023-08-11 09:04:43 +0200

    made prometheus optional

--- python/main.py
+++ python/main.py
@@ -1,17 +1,19 @@
-import glob
 import os
+import shutil
 
 from prometheus_client import CollectorRegistry, multiprocess
 
 # Setup prometheus for multiprocessing
-prom_dir = os.environ["PROMETHEUS_MULTIPROC_DIR"]
-for f in glob.glob(prom_dir + "/*"):
-    try:
-        os.remove(f)
-    except FileNotFoundError:
-        pass
-registry = CollectorRegistry()
-multiprocess.MultiProcessCollector(registry)
+prom_dir = (
+    os.environ["PROMETHEUS_MULTIPROC_DIR"]
+    if "PROMETHEUS_MULTIPROC_DIR" in os.environ
+    else None
+)
+if prom_dir is not None:
+    shutil.rmtree(prom_dir, ignore_errors=True)
+    os.makedirs(prom_dir, exist_ok=True)
+    registry = CollectorRegistry()
+    multiprocess.MultiProcessCollector(registry)
 
 from collections import defaultdict
 from typing import Annotated, List
