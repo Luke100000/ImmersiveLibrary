2bc001744ed4d66d3df0b50cb5e7ca7fe86d6eaf
Luke100000 <jonas.peche@aon.at>
2023-12-23 17:19:32 +0100

    top tags

--- python/main.py
+++ python/main.py
@@ -294,6 +294,15 @@ async def get_statistics():
     likes_count = await database.fetch_one("SELECT count(*) FROM likes")
     reports_count = await database.fetch_one("SELECT count(*) FROM reports")
 
+    top_tags = await database.fetch_all("""
+        SELECT tag
+        FROM tags
+        GROUP BY tag
+        HAVING count(*) > 10
+        ORDER BY count(*) desc
+        LIMIT 33
+    """)
+
     random_oid = await database.fetch_one(
         """
         SELECT oid
@@ -310,6 +319,7 @@ async def get_statistics():
 
     return {
         "oid": random_oid[0],
+        "top_tags": ", ".join([t[0] for t in top_tags[3:]]),
         "content": "{:,}".format(content_count[0]),
         "content_liked": "{:,}".format(content_count_liked[0]),
         "users": "{:,}".format(users_count[0]),
